69af66e9638fc430f943a95fd995724ead9e7353
Manuel Rodriguez <rodriguez.manu@gmail.com>
2016-08-24 01:10:57 -0300

    Adding up and down functionality and restrictions, also adding some global variables

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 (function() {
-    var jsonData = 'assets/data/data.json'
+    var jsonData = 'assets/data/data.json',
+        dropdownMenu = '.dropdown-menu',
+        mealsList = '.meals ul',
+        $downButton = $('.options .down'),
+        $upButton = $('.options .up');
 
     init();
 
     function init() {
         getData();
         listeners();
+        checkButtonsAvail();
     }
 
     function getData() {
@@ -20,7 +25,7 @@
     }
 
     function createMenuDropdown(data) {
-        var $dropdown = $('.dropdown-menu');
+        var $dropdown = $(dropdownMenu);
 
         $.each(data, function(index, value) {
             $dropdown.append(
@@ -30,7 +35,7 @@
         });
     }
     function createMealsMenu(data, index) {
-        var $meals = $('.meals ul'),
+        var $meals = $(mealsList),
             currentMeal = data[index].meals,
             titleMenu = data[index].title;
 
@@ -50,11 +55,37 @@
         downButton();
         upButton();
         changeDataMenu();
+        selectMeal();
+    }
+
+    function selectMeal() {
+        $(mealsList).on('click', 'li', function() {
+
+            $(mealsList + ' > li').removeClass('selected');
+            $(this).addClass('selected');
+            checkButtonsAvail();
+        });
+    }
+
+    function checkButtonsAvail() {
+        var mealsListN = $(mealsList + ' > li').length - 1,
+            mealSelectedIndex = $('.meals ul li.selected').index();
+
+        if (mealSelectedIndex === -1) {
+            $downButton.addClass('disabled');
+            $upButton.addClass('disabled');
+        } else if (mealSelectedIndex === 0) {
+            $upButton.addClass('disabled');
+            $downButton.removeClass('disabled');
+        } else if (mealsListN === mealSelectedIndex) {
+            $downButton.addClass('disabled');
+            $upButton.removeClass('disabled');
+        }
     }
 
     function changeDataMenu() {
-        $('.dropdown-menu').on('click', 'li', function() {
-            var index = $('.dropdown-menu li').index($(this));
+        $(dropdownMenu).on('click', 'li', function() {
+            var index = $(dropdownMenu + ' li').index($(this));
 
             $.getJSON(jsonData)
                 .done(function(response) {
@@ -66,15 +97,21 @@
         });
     }
 
-    function upButton(argument) {
-        $('.options .up').click(function() {
-            console.log('up');
+    function upButton() {
+        $upButton.click(function() {
+            var $mealSelected = $('.meals ul li.selected');
+
+            $mealSelected.insertBefore($mealSelected.prev());
+            checkButtonsAvail();
         });
     }
 
-    function downButton(argument) {
-        $('.options .down').click(function() {
-            console.log('down');
+    function downButton() {
+        $downButton.click(function() {
+            var $mealSelected = $('.meals ul li.selected');
+
+            $mealSelected.insertAfter($mealSelected.next());
+            checkButtonsAvail();
         });
     }
 })();
